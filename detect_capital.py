eadc3b513617771453c894faa1652a5093590dd8
unknown <bbang2358@gmail.com>
2023-02-09 20:23:08 +0545

    [add] adds solution to detect capital

---
+++ detect_capital.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def detectCapitalUse(self, word):
+        if word.isupper():
+            return True
+        if word.islower():
+            return True
+        if word.istitle():
+            return True
+        return False
